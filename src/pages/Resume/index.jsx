6af43c7f6a8f1f30756aba6ad4e4907642568e82
Mariah <111791344+mariahmcdaniel@users.noreply.github.com>
2023-03-28 15:42:39 -0400

    updated res

--- src/pages/Resume/index.jsx
+++ src/pages/Resume/index.jsx
@@ -23,7 +23,7 @@ const Resume = () => {
         type='button'
         onClick={(e) => {
           e.preventDefault();
-          window.location.href = 'https://drive.google.com/file/d/1NfZMVzecLvCtevWMVnx8FBbwv_1yKsq0/view?usp=sharing';
+          window.location.href = 'https://drive.google.com/file/d/1UEXszZSq6Z6N5085CENRFbznc-_ZFFcU/view?usp=sharing';
         }}>
         Download My Resume
       </button>
