a4e98fe8ea8c645558c7f555de63774afc598bbb
Mariah <111791344+mariahmcdaniel@users.noreply.github.com>
2023-05-08 21:29:22 -0400

    about update

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -11,7 +11,7 @@ export default function About() {
         </div>
         <div className='col-6 col-sm-10 introCol'>
           <p id="intro">
-            Hi, I'm Mariah! I am a full-stack web developer that is enthusiastically committed to continuous growth and adaptation in the ever-evolving world of programming. I find joy in picking up new skills and I love being able to apply my background in the creative arts and foreign languages while learning and writing code. Please <Link id="contactLink" to='/contact'>reach out</Link> if you'd like to connect! 
+            I am a full-stack web developer with a unique perspective and a passion for logic and problem solving. I am enthusiastic in my commitment to perpetual growth and adaptation as the world of programming is constantly evolving. I find joy in picking up new skills and attribute my success with this endeavor to a background in forein languages and the creative arts. while learning and writing code. Please <Link id="contactLink" to='/contact'>reach out</Link> if you'd like to connect! 
           </p>
         </div>
         <div className='col'>
