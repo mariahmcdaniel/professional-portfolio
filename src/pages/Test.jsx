7c865eac3e0dfd70848f29e1c71044ac326420d0
Mariah <111791344+mariahmcdaniel@users.noreply.github.com>
2024-03-18 13:05:28 -0400

    cleaned up test page

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -2,14 +2,21 @@ import React from 'react';
 import './style.css';
 import fleur from './fleur.mp4';
 import fleur2 from './fleur.webm';
+import logo24 from './2024LOGO500.png';
 
 export default function Test() {
     return (
-        <div className='row testCont g-0'>
+        <div className='row d-flex align-items-center testCont g-0'>
+            <h1 id='featTesth1'>Features Test Page</h1>
+            <div id='vidDiv' className='col-12 d-flex justify-content-center'>
             <video autoPlay muted loop playsinline>
                 <source src={fleur2} type="video/webm" />
                 <source src={fleur} type="video/mp4" />
             </video>
+            </div>
+            <div className='col-12 d-flex justify-content-center'>
+            <img id='logo24' src={logo24} alt='gemineye of the tiger logo'/>
+            </div>
         </div>
     );
 }
